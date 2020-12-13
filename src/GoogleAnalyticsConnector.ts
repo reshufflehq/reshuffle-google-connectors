fe2f75e4a1325e09be96a6d06a8d0127922fad72
christophe gachiniard <christophe@reshuffle.com>
2020-12-13 22:01:40 +1300

    GoogleAnalyticsConnector using universal-analytics package

--- src/GoogleAnalyticsConnector.ts
+++ src/GoogleAnalyticsConnector.ts
@@ -1,39 +1,33 @@
 import { Reshuffle, BaseConnector } from 'reshuffle-base-connector'
-import fetch from 'node-fetch'
+import Analytics, {AnalyticsInstance} from 'analytics'
+// @ts-ignore
+import googleAnalytics from '@analytics/google-analytics'
 
 
 export interface GoogleAnalyticsConnectorConfigOptions {
   GATrackingId: string // Google Analytics Tracking Identifier
-  cid: string // Anonymous Client Identifier
-  type?: string // Default is 'event'
 }
 
 export default class GoogleAnalyticsConnector extends BaseConnector<
     GoogleAnalyticsConnectorConfigOptions,
   null
   > {
+  private readonly analytics: AnalyticsInstance
 
   constructor(app: Reshuffle, options: GoogleAnalyticsConnectorConfigOptions, id?: string) {
-    options.type = options.type || 'event'
     super(app, options, id)
+
+    this.analytics = Analytics({
+      plugins: [
+        googleAnalytics({
+          trackingId: options.GATrackingId,
+        }),
+      ]
+    })
   }
 
-  async trackEvent(category: string, action: string, label: string, value: string): Promise<void> {
-    const params = {
-      v: '4', // API Version
-      tid: this.configOptions?.GATrackingId,
-      cid: this.configOptions?.cid,
-      t: this.configOptions?.type,
-      ec: category,
-      ea: action,
-      el: label,
-      ev: value,
-    }
-    const resp = await fetch('http://www.google-analytics.com/debug/collect', { method: 'POST', body: JSON.stringify(params) })
-
-    if(!resp.ok) {
-      this.app.getLogger().error('Reshuffle - Google Analytics Connector: fail to track event')
-    }
+  async trackEvent(eventName: string, payload: object): Promise<any> {
+   return this.analytics.track(eventName, payload)
   }
 }
 
